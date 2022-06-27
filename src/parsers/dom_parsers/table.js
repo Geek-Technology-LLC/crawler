1d3f1f0222b4a9e9bd1bbdfa22f2e9aa70941d7f
nyamkhuubuyanjargal <nyamkhuu@datascience.mn>
2022-06-27 18:19:34 +0800

    table parser done

--- src/parsers/dom_parsers/table.js
+++ src/parsers/dom_parsers/table.js
@@ -1,3 +1,4 @@
+import * as htmlParser from "node-html-parser";
 
 /**
  * @author B.Nyamkhuu
@@ -14,6 +15,27 @@ class TableParser {
   }
 
   parse() {
+    const root = htmlParser.parse(this.data);
+    const allTable = root.getElementsByTagName("table");
+    console.log(`${allTable.length} table found from HTML document`);
+    const result = [];
+    for (let i = 0; i < allTable.length; i+=1) {
+      const table = allTable[i];
+      const headers = table.getElementsByTagName("th");
+      const contents =table.getElementsByTagName("td");
+      const tableData = [];
+      const nth = contents.length / headers.length;
+
+      for (let i = 0 ; i < nth; i+=1) {
+        const singleData = {};
+        for (let j = 0; j < headers.length; j+=1) {
+          singleData[headers[j].innerText] = contents[j + i * headers.length].innerText;
+        }
+        tableData.push(singleData);
+      }
+      result.push(tableData);
+    }
+    return result;
   }
 
 }
