0781a3e3ab566dbea0d7d619dd5612fc6a2adec3
nyamkhuubuyanjargal <nyamkhuu@datascience.mn>
2022-07-23 23:02:53 +0800

    xpath way development started

---
+++ src/parsers/dom_parsers/xpath.js
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * @author B.Nyamkhuu
+ */
+class XPath {
+
+  /**
+   *
+   * @param {Object} params
+   */
+  constructor(params) {
+    this.params = params;
+  }
+
+  /**
+   *
+   * @param {String} data
+   * @returns {Object} result
+   */
+  toJSON(data) {
+    const result = {};
+    return result;
+  }
+}
+
+export default XPath;
